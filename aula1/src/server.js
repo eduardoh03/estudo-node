58fb000a0e323c39a9350acbae89cf90bb48217d
Eduardo <eduhenriquesrs@gmail.com>
2023-12-09 10:33:55 -0300

    Rotas de criação e listagem (Métodos HTTP) - Rocketseat

--- aula1/src/server.js
+++ aula1/src/server.js
@@ -1,10 +1,23 @@
+// Path: src/server.js
 //CommonJs => utiliza o require (pouco utilizad)
 import http from 'node:http';
-//ESModules =: import/export (modelo mais utilizado porém o node não suporta, para poder utilizar, deve-se adicionar
+//ESModules =: import/export (modelo mais utilizado, porém, o node não suporta, para poder utilizar, deve-se adicionar
 // "type": "module", ao package.json
 
-// Aplicações HTTP -> API's
+// Aplicações HTTP -> APIs
 const server = http.createServer((req, res) => {
+    const {method, url} = req
+    console.log(method, url)
+    if (method === 'GET' && url === '/users') {
+        return res.end('Listagem de usuários')
+    }
+    if (method === 'POST' && url === '/users') {
+        return res.end('Criar usuário')
+    }
     return res.end("Hello World")
 })
 server.listen(3333)
+console.log('Server started at port 3333')
+
+// Para rodar o servidor, basta abrir o terminal e digitar: node src/server.js
+// Para parar o servidor, basta apertar o Ctrl + C no terminal
