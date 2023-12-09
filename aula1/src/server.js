7600c1483cc0529b8959b136422079fc0b2c82d2
Eduardo <eduhenriquesrs@gmail.com>
2023-12-09 11:09:55 -0300

    refactor: Ensure route access based on correct HTTP method capitalization
    Adjusted the routes object to match the correct HTTP method capitalization ('GET', 'POST', etc.) to resolve the issue where routes[method] was returning 'undefined' due to case sensitivity. Now the server correctly accesses the defined routes based on the incoming request's HTTP method.

--- aula1/src/server.js
+++ aula1/src/server.js
@@ -4,20 +4,42 @@ import http from 'node:http';
 //ESModules =: import/export (modelo mais utilizado, porém, o node não suporta, para poder utilizar, deve-se adicionar
 // "type": "module", ao package.json
 
-// Aplicações HTTP -> APIs
+const routes = {
+    GET: {
+        '/': (req, res) => {
+            return res.end('Hello World')
+        },
+        '/users': (req, res) => {
+            return res.end('Listagem de usuários')
+        }
+    },
+    POST: {
+        '/users': (req, res) => {
+            return res.end('Criar usuário')
+        }
+    }
+};
+
+
 const server = http.createServer((req, res) => {
-    const {method, url} = req
-    console.log(method, url)
-    if (method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários')
+    const { method, url } = req;
+    console.log(method)
+    // Verifica se o método existe nas rotas
+    if (routes[method]) {
+        const handler = routes[method][url];
+        // Verifica se o manipulador para a rota específica existe
+        if (handler) {
+            handler(req, res);
+        } else {
+            res.end('Rota não encontrada');
+        }
+    } else {
+        res.end('Método não permitido');
     }
-    if (method === 'POST' && url === '/users') {
-        return res.end('Criar usuário')
-    }
-    return res.end("Hello World")
-})
-server.listen(3333)
-console.log('Server started at port 3333')
+});
+
+server.listen(3333);
+console.log('Servidor iniciado na porta 3333');
 
 // Para rodar o servidor, basta abrir o terminal e digitar: node src/server.js
 // Para parar o servidor, basta apertar o Ctrl + C no terminal
